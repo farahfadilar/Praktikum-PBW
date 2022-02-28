db4ce49375b175315562b55bbf6b1aaa52c41a54
farahfadilar <76088243+farahfadilar@users.noreply.github.com>
2022-03-01 00:10:38 +0700

    Tugas Praktikum PBW

--- Pertemuan 3 - JavaScript Dasar/Deret Fibonacci.js
+++ Pertemuan 3 - JavaScript Dasar/Deret Fibonacci.js
@@ -4,11 +4,11 @@ function fibonacci(suku)
 	var suku2 = 1;
 	var sukuN;
 	
-    console.log("Deret Fibonacci hingga Suku ke-"+suku+" :");
+	console.log("Deret Fibonacci hingga Suku ke-"+suku+" :");
 
 	for (var i = 0; i < suku; i++)
 	{
-        console.log(suku1);
+    	console.log(suku1);
 		sukuN = suku1 + suku2;
 		suku1 = suku2;
 		suku2 = sukuN;
